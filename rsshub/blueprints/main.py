5cbbcdc971a7e52c48e4ea24510cea8ff27262c5
2502302255 <49912823+2502302255@users.noreply.github.com>
2019-04-23 23:11:44 +0800

    Add files via upload

--- rsshub/blueprints/main.py
+++ rsshub/blueprints/main.py
@@ -50,3 +50,8 @@ def ctolib_topics(category=''):
 def infoq_recommend():
     from rsshub.spiders.infoq.recommend import ctx
     return render_template('main/atom.xml', **filter_content(ctx()))
+
+@bp.route('/52pojie/52pojie')
+def 52pojie_52pojie():
+    from rsshub.spiders.52pojie.52pojie import ctx
+    return render_template('main/atom.xml', **filter_content(ctx()))
